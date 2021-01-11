6b5948835e4b7a5fc0a19c61cd0c7d925c2ce520
hp <adjilsonsantana@gmail.com>
2021-01-11 07:46:18 +0100

    GET order

--- BackEnd/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ BackEnd/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -7,23 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.dsdeliver.dto.ProductDTO;
-import com.devsuperior.dsdeliver.entities.Product;
-import com.devsuperior.dsdeliver.repositories.ProductRepository;
-
-
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.repositories.OrderRepository;
 
 /*We have to inject the component product service in the tontroller, because of this we have to create @service*/
 @Service
-public class ProductService {
+public class OrderService {
 	
 	@Autowired
-	private ProductRepository repository;
+	private OrderRepository repository;
 	
 	@Transactional(readOnly= true)
-	public List<ProductDTO> findAll(){
-		List<Product> list = repository.findAllByOrderByNameAsc();
-		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());		
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrderWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());		
 	}
 
 }
